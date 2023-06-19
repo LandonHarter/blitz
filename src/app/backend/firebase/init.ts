21328341c6bc41191e045d290a7eb08f81ae9298
LandonHarter <landonharter@outlook.com>
2023-06-19 10:24:39 -0400

    environment variables

--- src/app/backend/firebase/init.ts
+++ src/app/backend/firebase/init.ts
@@ -5,14 +5,17 @@ import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
 import { Analytics, getAnalytics } from "firebase/analytics";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 const firebaseConfig = {
-  apiKey: "AIzaSyB1QnUJVyf0hm3YOKSVo_R3iv300LydjMc",
-  authDomain: "blitz-c4a7c.firebaseapp.com",
-  projectId: "blitz-c4a7c",
-  storageBucket: "blitz-c4a7c.appspot.com",
-  messagingSenderId: "811288720110",
-  appId: "1:811288720110:web:69d6f1399218d73c31b9ff",
-  measurementId: "G-QDE84XR9TL"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_API_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
