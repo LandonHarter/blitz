9fdb7de8ebc92689a744a8e5a238a1b39f2cebe1
LandonHarter <landonharter@outlook.com>
2023-06-19 22:00:16 -0400

    stop using env variables

--- src/app/backend/firebase/init.ts
+++ src/app/backend/firebase/init.ts
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app";
+import { FirebaseOptions, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
@@ -7,6 +7,7 @@ import { getStorage } from "firebase/storage";
 import dotenv from "dotenv";
 dotenv.config();
 
+/*
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -16,6 +17,17 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};*/
+
+const firebaseConfig:FirebaseOptions = {
+  apiKey: "AIzaSyB1QnUJVyf0hm3YOKSVo_R3iv300LydjMc",
+  authDomain: "blitz-c4a7c.firebaseapp.com",
+  projectId: "blitz-c4a7c",
+  storageBucket: "blitz-c4a7c.appspot.com",
+  messagingSenderId: "811288720110",
+  appId: "1:811288720110:web:69d6f1399218d73c31b9ff",
+  measurementId: "G-QDE84XR9TL",
+  databaseURL: "https://blitz-c4a7c-default-rtdb.firebaseio.com"
 };
 
 export const app = initializeApp(firebaseConfig);
