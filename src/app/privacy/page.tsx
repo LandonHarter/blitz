66c3aff9f08ef73018b4f30268537aa06b000e84
LandonHarter <landonharter@outlook.com>
2023-06-26 12:24:05 -0400

    tos + privacy policy

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,80 @@
+'use client'
+
+import Footer from '@/components/footer/footer';
+import styles from './page.module.css';
+
+export default function PrivacyPage() {
+    return(
+        <div>
+            <div className={styles.privacy_container}>
+                <h1 className={styles.title}>Privacy Policy</h1>
+                <h3 className={styles.subtitle}>Effective Date: 6/26/2023</h3>
+                <div className={styles.divider} />
+                <div className={styles.privacy}>
+                    <p>
+                        1. Information We Collect
+                        <p className={styles.section_content}>
+                            <p className={styles.section_content}>
+                                1.1 Personal Information: We may collect certain personally identifiable information (&quot;Personal Information&quot;) from you when you voluntarily provide it to us. This includes information such as your name, email address, and any other information you provide during the registration process or through support or request channels.
+                            </p>
+                            <p className={styles.section_content}>
+                            1.2 Usage Data: We may collect non-personal information about how you interact with our website and services. This may include information such as your IP address, browser type, device information, referring/exit pages, and operating system. We may also collect information about your usage patterns and preferences.
+                            </p>
+                        </p>
+                    </p>
+                    <p>
+                        2. Use of Information
+                        <p className={styles.section_content}>
+                            <p className={styles.section_content}>
+                            2.1 Providing Services: We use the information we collect to provide, personalize, maintain, and improve our website and services. This includes delivering study tools and classroom tools, customizing content, and responding to your requests.
+                            </p>
+                            <p className={styles.section_content}>
+                            2.2 Communication: We may use your Personal Information to communicate with you about our services, updates, newsletters, and promotional materials. You can opt-out of receiving such communications at any time by following the instructions provided in the communication or by contacting us directly.
+                            </p>
+                            <p className={styles.section_content}>
+                            2.3 Support and Feedback: If you contact us for support or submit feedback, we may collect and process the information provided to assist you and improve our services.
+                            </p>
+                            <p className={styles.section_content}>
+                            2.4 Aggregated Data: We may aggregate and anonymize your information to analyze trends, monitor usage patterns, and improve our website and services. This aggregated data will not personally identify you.
+                            </p>
+                        </p>
+                    </p>
+                    <p>
+                        3. Disclosure of Information
+                        <p className={styles.section_content}>
+                            <p className={styles.section_content}>
+                            3.1 Service Providers: We may share your information with trusted third-party service providers who assist us in operating our website, providing our services, and conducting our business. These providers are obligated to protect your information and may only use it for the purposes specified by us.
+                            </p>
+                            <p className={styles.section_content}>
+                            3.2 Legal Compliance: We may disclose your information if required by law, court order, or governmental authority, or if we believe that such action is necessary to comply with applicable laws, protect our rights, or investigate fraud or security issues.
+                            </p>
+                            <p className={styles.section_content}>
+                            3.3 Business Transfers: In the event of a merger, acquisition, or sale of all or a portion of our assets, your information may be transferred as part of the transaction. We will notify you via email and/or prominent notice on our website of any change in ownership or uses of your Personal Information.
+                            </p>
+                        </p>
+                    </p>
+                    <p>
+                        4. Data Security
+                        <p className={styles.section_content}>
+                        We implement reasonable security measures to protect your information from unauthorized access, alteration, disclosure, or destruction. However, no method of transmission over the internet or electronic storage is 100% secure, and we cannot guarantee absolute security.
+                        </p>
+                    </p>
+                    <p>
+                        5. Third-Party Websites
+                        <p className={styles.section_content}>
+                        Our website may contain links to third-party websites. This Privacy Policy does not apply to those websites, and we are not responsible for the privacy practices or content of such websites. We encourage you to review the privacy policies of those third-party websites before providing any personal information.
+                        </p>
+                    </p>
+                    <p>
+                        6. Changes to this Policy
+                        <p className={styles.section_content}>
+                        We may update this Privacy Policy from time to time. Any changes will be effective immediately upon posting the revised Privacy Policy on our website.
+                        </p>
+                    </p>
+                </div>
+            </div>
+
+            <Footer />
+        </div>
+    );
+}
---
+++ src/app/terms/page.tsx
@@ -0,0 +1,106 @@
+'use client'
+
+import Footer from '@/components/footer/footer';
+import styles from './page.module.css';
+
+export default function TOSPage() {
+    return(
+        <div>
+            <div className={styles.terms_container}>
+                <h1 className={styles.title}>Terms of Service</h1>
+                <h3 className={styles.subtitle}>Effective Date: 6/26/2023</h3>
+                <div className={styles.divider} />
+                <div className={styles.terms}>
+                    <p>
+                        1. Acceptance of Terms of Service
+                        <p className={styles.section_content}>
+                        By accessing or using the Blitz website, you confirm that you have read, understood, and agree to be bound by these Terms. If you do not agree to these Terms, you may not use the Service.
+                        </p>
+                    </p>
+                    <p>
+                        2. Description of Service
+                        <p className={styles.section_content}>
+                        Blitz is an educational platform that provides live games and study tools to enhance your learning experience. The Platform offers various features, including but not limited to, live game hosting, study materials, interactive lessons, and community forums.
+                        </p>
+                    </p>
+                    <p>
+                        3. Registration and Account
+                        <p className={styles.section_content}>
+                        Blitz is an educational platform that provides live games and study tools to enhance your learning experience. The Platform offers various features, including but not limited to, live game hosting, study materials, interactive lessons, and community forums.
+                        </p>
+                    </p>
+                    <p>
+                        4. User Conduct
+                        <p className={styles.section_content}>
+                        When using Blitz, you agree to abide by the following guidelines:
+                        You will not use the Service for any unlawful or unauthorized purposes.
+                        You will not transmit any viruses, worms, or any other destructive code.
+                        You will not interfere with or disrupt the Service or servers or networks connected to the Service.
+                        You will not engage in any activity that may harm or exploit minors.
+                        You will not collect or store personal information of other users without their consent.
+                        You will not impersonate any person or entity or misrepresent your affiliation with any person or entity.
+                        You will not use the Service in any manner that could disable, overburden, or impair the Service.
+                        You will not attempt to gain unauthorized access to any part of the Service or its related systems or networks.
+                        </p>
+                    </p>
+                    <p>
+                        5. Intellectual Property Rights
+                        <p className={styles.section_content}>
+                        Blitz is an educational platform that provides live games and study tools to enhance your learning experience. The Platform offers various features, including but not limited to, live game hosting, study materials, interactive lessons, and community forums.
+                        </p>
+                    </p>
+                    <p>
+                        6. Privacy
+                        <p className={styles.section_content}>
+                        Your privacy is important to us. Please refer to our Privacy Policy for information on how we collect, use, and disclose personal information.
+                        </p>
+                    </p>
+                    <p>
+                        7. Third Party Content
+                        <p className={styles.section_content}>
+                        The Service may contain links to third-party websites or resources that are not owned or controlled by Blitz. Blitz does not endorse or assume any responsibility for the content, privacy policies, or practices of any third-party websites. You acknowledge and agree that Blitz shall not be liable for any damages or losses arising from your use of any third-party websites.
+                        </p>
+                    </p>
+                    <p>
+                        8. Limitation and Liability
+                        <p className={styles.section_content}>
+                        To the maximum extent permitted by law, Blitz and its affiliates, officers, directors, employees, agents, and licensors shall not be liable for any direct, indirect, incidental, special, consequential, or exemplary damages, including but not limited to damages for loss of profits, goodwill, use, data, or other intangible losses, arising out of or in connection with your use of the Service.
+                        </p>
+                    </p>
+                    <p>
+                        9. Termination
+                        <p className={styles.section_content}>
+                        Blitz reserves the right to suspend, terminate, or restrict your access to the Service at any time, with or without cause, and without prior notice or liability. Upon termination, all licenses and rights granted to you under these Terms will immediately cease, and you must cease all use of the Service.
+                        </p>
+                    </p>
+                    <p>
+                        10. Modifications to the Service
+                        <p className={styles.section_content}>
+                        Blitz reserves the right to modify, suspend, or discontinue the Service at any time without prior notice or liability. We may also impose limits on certain features and services or restrict your access to parts or all of the Service without notice or liability.
+                        </p>
+                    </p>
+                    <p>
+                        11. Changes to the Terms of Service
+                        <p className={styles.section_content}>
+                        Blitz may revise these Terms from time to time, and any changes will be effective upon posting the updated Terms on the Blitz website. It is your responsibility to review these Terms periodically. Your continued use of the Service after any modifications signifies your acceptance of the revised Terms.
+                        </p>
+                    </p>
+                    <p>
+                        12. Entire Agreement
+                        <p className={styles.section_content}>
+                        hese Terms constitute the entire agreement between you and Blitz regarding the use of the Service and supersede any prior or contemporaneous agreements, communications, and proposals, whether oral or written, between you and Blitz.
+                        </p>
+                    </p>
+                    <p>
+                        13. Contact Information
+                        <p className={styles.section_content}>
+                        If you have any questions or concerns regarding these Terms, please contact me at landonharter@outlook.com
+                        </p>
+                    </p>
+                </div>
+            </div>
+
+            <Footer />
+        </div>
+    );
+}
