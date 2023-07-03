b464bdd0298e1473345f2cccb77b1ccb8cf7978d
LandonHarter <landonharter@outlook.com>
2023-07-03 16:56:58 -0400

    update animations

--- src/app/animation/animation.ts
+++ src/app/animation/animation.ts
@@ -10,6 +10,22 @@ export const fade:Animation = {
     }
 };
 
+export const dropdown:Animation = {
+    initial: {
+        opacity: 0,
+        scaleY: 0.8,
+        transformOrigin: 'top',
+    },
+    animate: {
+        opacity: 1,
+        scaleY: 1,
+    },
+    exit: {
+        opacity: 0,
+        scaleY: 0.8,
+    }
+};
+
 export interface Animation {
     initial:any;
     animate:any;
--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -4,19 +4,21 @@ import Link from "next/link";
 import styles from "./header.module.css";
 
 import Image from "next/image";
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 import { signInWithGithub, signInWithGoogle, signInWithMicrosoft, signOut } from "@baas/login";
 import useCurrentUser from "@hooks/useCurrentUser";
 import Loading from "@components/loading/loading";
 import useOutsideClick from "@/hooks/useOutsideClick";
 
-import Popup from "../popup/popup";
+import Popup from "@components/popup/popup";
+import { AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
+import AnimationDiv from "@/animation/AnimationDiv";
+import { dropdown } from "@/animation/animation";
 
 export default function Header() {
     const [showLogin, setShowLogin] = useState(false);
-
     const [avatarDropdown, setAvatarDropdown] = useState(false);
-    const [avatarDropdownVisible, setAvatarDropdownVisible] = useState(false);
 
     const avatarDropdownRef = useRef(null);
     const loginModalRef = useRef(null);
@@ -25,51 +27,6 @@ export default function Header() {
     useOutsideClick(loginModalRef, () => setShowLogin(false));
 
     const { currentUser, signedIn, userLoading } = useCurrentUser();
-
-    useEffect(() => {
-        if (avatarDropdown) setAvatarDropdownVisible(true);
-        else setTimeout(() => setAvatarDropdownVisible(false), 100);
-    }, [avatarDropdown]);
-
-    const Avatar = () => {
-        return(
-            <div className={styles.avatar_container}>
-                <Image src={currentUser.pfp} alt="avatar" className={styles.avatar_image} width={42} height={42} onClick={() => {
-                    setAvatarDropdown(!avatarDropdown);
-                }} />
-
-                <div className={`${styles.avatar_dropdown} ${avatarDropdownVisible ? (avatarDropdown ? styles.avatar_dropdown_visible : styles.avatar_dropdown_hidden_animation) : styles.avatar_dropdown_hidden}`} 
-                ref={avatarDropdownRef}>
-                    <Link href={`/profile/${currentUser.uid}`} className={styles.avatar_dropdown_option} onClick={() => {
-                        setAvatarDropdown(false);
-                    }}>
-                        <Image src='/images/icons/user.png' alt='user' width={25} height={25} />
-                        <p>PROFILE</p>
-                    </Link>
-                    <Link href='/my-sets' className={styles.avatar_dropdown_option} onClick={() => {
-                        setAvatarDropdown(false);
-                    }}>
-                        <Image src='/images/icons/mysets.png' alt='user' width={25} height={25} />
-                        <p>MY SETS</p>
-                    </Link>
-                    <Link href='/settings' className={styles.avatar_dropdown_option} onClick={() => {
-                        setAvatarDropdown(false);
-                    }}>
-                        <Image src='/images/icons/settings.png' alt='user' width={25} height={25} />
-                        <p>SETTINGS</p>
-                    </Link>
-                    <div className={styles.avatar_dropdown_option} onClick={() => {
-                        signOut();
-                        window.location.href = "/";
-                        setAvatarDropdown(false);
-                    }}>
-                        <Image src='/images/icons/logout.png' alt='user' width={25} height={25} />
-                        <p style={{color: "red"}}>SIGN OUT</p>
-                    </div>
-                </div>
-            </div>
-        );
-    }
 
     if (userLoading) {
         return(<Loading />);
@@ -96,28 +53,63 @@ export default function Header() {
                         <div>
                             <button className={styles.signin_button} onClick={() => setShowLogin(true)}>Log In</button>
                         </div> : 
-                        <div>
-                            <Avatar />
+                        <div className={styles.avatar_container}>
+                            <Image src={currentUser.pfp} alt="avatar" className={styles.avatar_image} width={42} height={42} onClick={() => {
+                                setAvatarDropdown(!avatarDropdown);
+                            }} />
+
+                            <AnimatePresence initial={false} mode='wait'>
+                                {avatarDropdown &&
+                                    <AnimationDiv animation={dropdown} duration={0.1} className={styles.avatar_dropdown}>
+                                        <Link href={`/profile/${currentUser.uid}`} className={styles.avatar_dropdown_option} onClick={() => {
+                                            setAvatarDropdown(false);
+                                        }}>
+                                            <Image src='/images/icons/user.png' alt='user' width={25} height={25} />
+                                            <p>PROFILE</p>
+                                        </Link>
+                                        <Link href='/my-sets' className={styles.avatar_dropdown_option} onClick={() => {
+                                            setAvatarDropdown(false);
+                                        }}>
+                                            <Image src='/images/icons/mysets.png' alt='user' width={25} height={25} />
+                                            <p>MY SETS</p>
+                                        </Link>
+                                        <Link href='/settings' className={styles.avatar_dropdown_option} onClick={() => {
+                                            setAvatarDropdown(false);
+                                        }}>
+                                            <Image src='/images/icons/settings.png' alt='user' width={25} height={25} />
+                                            <p>SETTINGS</p>
+                                        </Link>
+                                        <div className={styles.avatar_dropdown_option} onClick={() => {
+                                            signOut();
+                                            setAvatarDropdown(false);
+                                        }}>
+                                            <Image src='/images/icons/logout.png' alt='user' width={25} height={25} />
+                                            <p style={{color: "red"}}>SIGN OUT</p>
+                                        </div>
+                                    </AnimationDiv>
+                                }   
+                            </AnimatePresence>
                         </div>
                     }
                 </div>
             </div>
+
             <Popup open={showLogin} setOpen={setShowLogin} exitButton>
                 <h1 className={styles.login_modal_title}>Login</h1>
                 <p className={styles.login_modal_subtitle}>Sign in to start creating!</p>
 
-                <button className={styles.provider_button} onClick={() => signInWithGoogle().finally(() => setShowLogin(false))}>
+                <motion.button whileTap={{ scale: 0.95 }} transition={{ duration: 0.05 }} className={styles.provider_button} onClick={() => signInWithGoogle().finally(() => setShowLogin(false))}>
                     <Image src='/images/providers/google.png' alt="google" width={25} height={25} />
                     Sign in with Google
-                </button>
-                <button className={styles.provider_button} onClick={() => signInWithMicrosoft().finally(() => setShowLogin(false))}>
+                </motion.button>
+                <motion.button whileTap={{ scale: 0.95 }} transition={{ duration: 0.05 }} className={styles.provider_button} onClick={() => signInWithMicrosoft().finally(() => setShowLogin(false))}>
                     <Image src='/images/providers/microsoft.png' alt="microsoft" width={25} height={25} />
                     Sign in with Microsoft
-                 </button>
-                <button className={styles.provider_button} onClick={() => signInWithGithub().finally(() => setShowLogin(false))}>
+                </motion.button>
+                <motion.button whileTap={{ scale: 0.95 }} transition={{ duration: 0.05 }} className={styles.provider_button} onClick={() => signInWithGithub().finally(() => setShowLogin(false))}>
                     <Image src='/images/providers/github.png' alt="github" width={25} height={25} />
                     Sign in with Github
-                </button>
+                </motion.button>
                 <div style={{ marginBottom: 30 }} />
             </Popup>
         </div>
