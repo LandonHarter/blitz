4ddc5175dbd2733e2b8f25f01713131fdbef39ea
LandonHarter <landonharter@outlook.com>
2023-07-17 11:12:59 -0400

    apply for teacher verified account

---
+++ src/app/apply/teacher/requisites/page.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import styles from './page.module.css';
+
+export default function ApplyTeacherRequisitesPage() {
+    return (
+        <div className={styles.requisites_container}>
+            <h1 className={styles.title}>Teacher Application Requisites</h1>
+            <h3 className={styles.subtitle}>Effective Date: 7/17/2023</h3>
+            <div className={styles.divider} />
+            <div className={styles.requisites}>
+                <p>
+                    1. School Issued Email
+                    <p className={styles.section_content}>
+                        <p className={styles.section_content}>
+                            1.1 Teacher verification requires the account to use a school issued email address.
+                        </p>
+                    </p>
+                </p>
+                <p>
+                    2. Employment
+                    <p className={styles.section_content}>
+                        <p className={styles.section_content}>
+                            2.1 You must be actively employed by a school or educational institution.
+                        </p>
+                        <p className={styles.section_content}>
+                            2.2 Accepted forms of employment include but are not limited to: Teacher, Professor, Instructor, Tutor.
+                        </p>
+                    </p>
+                </p>
+            </div>
+        </div>
+    )
+};
