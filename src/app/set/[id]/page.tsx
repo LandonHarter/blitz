90fa94e2e9f80b75040e475feacbc0b51f7daf04
LandonHarter <landonharter@outlook.com>
2023-07-19 23:49:03 -0400

    twitter seo on sets

--- src/app/set/[id]/page.tsx
+++ src/app/set/[id]/page.tsx
@@ -10,12 +10,6 @@ export const metadata: Metadata = {
 export default function SetPage() {
     return (
         <>
-            <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:site" content="@blitz_studio" />
-            <meta name="twitter:title" content="Set • Blitz" />
-            <meta name="twitter:description" content="A set of questions to study." />
-            <meta name="twitter:image" content="https://www.rd.com/wp-content/uploads/2016/01/04-dog-breeds-dalmation.jpg" />
-
             <SetContent />
         </>
     );
