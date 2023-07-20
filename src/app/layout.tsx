41395dba16d43a05466cf341ecca24acd4262051
LandonHarter <landonharter@outlook.com>
2023-07-19 20:17:55 -0400

    add metadata to pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { Metadata } from 'next';
 import './globals.css';
 import RootLayoutContent from './rootlayoutcontent';
 
-export const metadata:Metadata = {
+export const metadata: Metadata = {
   title: 'Blitz',
   description: 'Host and create fun and educational live games for students',
 };
