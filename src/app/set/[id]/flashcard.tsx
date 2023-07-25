a9a35889eba668064ac0ee08e47f9ba2215c5f4e
LandonHarter <landonharter@outlook.com>
2023-07-25 12:20:08 -0400

    fix flashcard overflow

--- src/app/set/[id]/flashcard.tsx
+++ src/app/set/[id]/flashcard.tsx
@@ -5,7 +5,7 @@ import { Question } from "@/backend/live/set";
 
 export default function FlashcardAnswerDropdown(props: { question: Question }) {
     return (
-        <div className={styles.options_container}>
+        <div className={styles.flashcard_answer_container}>
             <div className={`${styles.option}`}>
                 <h1>{props.question.options[0].optionData.answer}</h1>
             </div>
