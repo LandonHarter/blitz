c3703c73440082bc5a7a7df592666de22fe406f7
LandonHarter <landonharter@outlook.com>
2023-07-27 18:14:29 -0400

    edit past blurts

--- src/app/study/[id]/methods/blurting/blurting.tsx
+++ src/app/study/[id]/methods/blurting/blurting.tsx
@@ -16,6 +16,7 @@ import { StudyMethod, updateStudyData } from "@/backend/firebase/study";
 import UserContext from "@/context/usercontext";
 import RequireSignInStudyMethod from "../needsignin";
 import StudyMethodContainer from "../studymethod";
+import { EditSVG } from "@/svg";
 
 export default function BlurtingStudyMethod(props: { set: any, studyData: any, setStudyData: Dispatch<SetStateAction<any>> }) {
     const [writing, setWriting] = useState(true);
@@ -47,6 +48,7 @@ export default function BlurtingStudyMethod(props: { set: any, studyData: any, s
                     <div className={styles.blurt_new} onClick={() => {
                         setWriting(true);
                         setCurrentBlurt(undefined);
+                        setBlurtText('');
                     }}>
                         <h1>+</h1>
                     </div>
@@ -56,30 +58,56 @@ export default function BlurtingStudyMethod(props: { set: any, studyData: any, s
                     <AnimatePresence mode='wait'>
                         {writing &&
                             <AnimationDiv className={styles.new_blurt_section} animation={fade} duration={0.1}>
-                                <textarea className={styles.blurt_input} placeholder="Write your blurt here..." onChange={(e) => {
+                                <textarea className={styles.blurt_input} placeholder="Write your blurt here..." value={blurtText} onChange={(e) => {
                                     setBlurtText(e.target.value);
                                 }} />
                                 <button className={styles.save_blurt} onClick={async () => {
                                     if (blurtText === '') return;
 
-                                    const newBlurt = {
-                                        text: blurtText,
-                                        createdAt: Timestamp.now()
-                                    };
-                                    props.setStudyData({
-                                        ...props.studyData,
-                                        blurting: {
+                                    if (!currentBlurt) {
+                                        const newBlurt = {
+                                            text: blurtText,
+                                            createdAt: Timestamp.now()
+                                        };
+                                        props.setStudyData({
+                                            ...props.studyData,
+                                            blurting: {
+                                                blurts: [...props.studyData.blurts, newBlurt]
+                                            }
+                                        });
+
+                                        setCurrentBlurt(newBlurt);
+                                        setWriting(false);
+                                        setBlurtText('');
+
+                                        await updateStudyData(props.set.id, StudyMethod.Blurting, currentUser.uid, {
                                             blurts: [...props.studyData.blurts, newBlurt]
-                                        }
-                                    });
+                                        });
+                                    } else {
+                                        const newBlurts = [...props.studyData.blurts];
+                                        const index = newBlurts.indexOf(currentBlurt);
+                                        newBlurts[index] = {
+                                            ...currentBlurt,
+                                            text: blurtText
+                                        };
+                                        props.setStudyData({
+                                            ...props.studyData,
+                                            blurting: {
+                                                blurts: newBlurts
+                                            }
+                                        });
 
-                                    setCurrentBlurt(newBlurt);
-                                    setWriting(false);
-                                    setBlurtText('');
+                                        setCurrentBlurt({
+                                            ...currentBlurt,
+                                            text: blurtText
+                                        });
+                                        setWriting(false);
+                                        setBlurtText('');
 
-                                    await updateStudyData(props.set.id, StudyMethod.Blurting, currentUser.uid, {
-                                        blurts: [...props.studyData.blurts, newBlurt]
-                                    });
+                                        await updateStudyData(props.set.id, StudyMethod.Blurting, currentUser.uid, {
+                                            blurts: newBlurts
+                                        });
+                                    }
                                 }}>Save</button>
                             </AnimationDiv>
                         }
@@ -87,7 +115,13 @@ export default function BlurtingStudyMethod(props: { set: any, studyData: any, s
                     <AnimatePresence mode='wait'>
                         {!writing &&
                             <AnimationDiv className={styles.selected_blurt_section} animation={fade} duration={0.1}>
-                                <h1 className={styles.blurt_title}>{formatTimestampDate(currentBlurt.createdAt)}</h1>
+                                <div className={styles.blurt_top_row}>
+                                    <h1 className={styles.blurt_title}>{formatTimestampDate(currentBlurt.createdAt)}</h1>
+                                    <EditSVG className={styles.edit_blurt} onClick={() => {
+                                        setWriting(true);
+                                        setBlurtText(currentBlurt.text);
+                                    }} />
+                                </div>
                                 <ReactMarkdown className={styles.blurt_content} components={{
                                     p: ({ children }) => <p className={styles.blurt_p}>{children}</p>,
                                     h1: ({ children }) => <h1 className={styles.blurt_h1}>{children}</h1>,
