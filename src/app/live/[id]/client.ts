6b37b63a6cfe5e2ca36267bb698ac712ed086c44
LandonHarter <landonharter@outlook.com>
2023-07-31 15:54:55 -0400

    refactor client

---
+++ src/app/live/[id]/client.ts
@@ -0,0 +1,32 @@
+import { User } from "@/backend/firebase/user";
+import { Unsubscribe, get, ref } from "firebase/database";
+import { realtimeDb } from "@/backend/firebase";
+import { leaveGame } from "@/backend/live/game";
+
+export const initClient = async (gameId: string, currentUser: User) => {
+    const gameRef = ref(realtimeDb, `live-games/${gameId}/users/${currentUser.uid}`);
+    if (!(await get(ref(realtimeDb, `live-games/${gameId}`))).exists()) {
+        return {
+            userInGame: false,
+            error: true,
+        }
+    }
+
+    const snapshot = await get(gameRef);
+    const userInGame = snapshot.exists();
+
+    return {
+        userInGame,
+        error: false,
+    }
+}
+
+export const unloadCallback = (gameId: string, currentUser: User, unsubscribeEvent: Unsubscribe, unsubscribeNewPlayer: Unsubscribe, unsubscribeLeavePlayer: Unsubscribe) => {
+    return async () => {
+        unsubscribeEvent();
+        unsubscribeNewPlayer();
+        unsubscribeLeavePlayer();
+
+        await leaveGame(gameId, currentUser);
+    };
+};
